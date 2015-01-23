ca6dd83730519bf5991f37e02144895e22084fa5
Alexander <alexander.lea@gmail.com>
2015-01-23 20:39:29 +0000

    Updated Database context

--- SOFT331_Assignment/Models/DatabaseContext.cs
+++ SOFT331_Assignment/Models/DatabaseContext.cs
@@ -8,16 +8,17 @@ namespace SOFT331_Assignment.Models
 {
     public class DatabaseContext : DbContext
     {
-        public DbSet<TicketType> EventTypes { get; set; }
+        
         public DbSet<Fare> Fares { get; set; }
+        public DbSet<Event> Events { get; set; }
         public DbSet<FareType> FareTypes { get; set; }
-        public DbSet<Journey> Journies { get; set; }
-        public DbSet<JourneyType> JourneyTypes { get; set; }
+        public DbSet<Journey> Journies { get; set; }        
         public DbSet<Station> Stations { get; set; }
+        public DbSet<Stop> Stops { get; set; }
         public DbSet<Ticket> Tickets { get; set; }
+        public DbSet<TicketType> TicketTypes { get; set; }
         public DbSet<Train> Trains { get; set; }
         public DbSet<Traveller> Travellers { get; set; }
-
 
         protected override void OnModelCreating(DbModelBuilder modelBuilder)
         {
