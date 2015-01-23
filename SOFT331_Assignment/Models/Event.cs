ac90ff980c239c21851dadb52f8f7969f3061573
Alexander <alexander.lea@gmail.com>
2015-01-23 20:13:13 +0000

    event

---
+++ SOFT331_Assignment/Models/Event.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace SOFT331_Assignment.Models
+{
+    public class Event
+    {
+        //+ EventID: int
+        //+ Name: string
+        //+ Description: string
+
+        [Required]        
+        public int EventID { get; set; }
+
+        [Required(ErrorMessage="Event must have a name")]
+        [DisplayName("Event Name")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Event must have a description")]
+        [DisplayName("Event Description")]
+        public string Description { get; set; }
+
+        public Event()
+        {
+           
+        }
+    }
+}
